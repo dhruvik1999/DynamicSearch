d7a8cd8767e2e5be711983f4269b0ad4c0b8b581
Dhruvik Navadiya <navadiyadhruvik2@gmail.com>
2019-08-31 23:19:03 +0530

    rec Levenshtein distance added

--- Search.py
+++ Search.py
@@ -1,9 +1,34 @@
 #Dynamic searc for given text arad
 import csv
 
-def keyWordMatcher():
-	pass
+def getMin(a,b,c):
+	if a<=b and a<=c:
+		return a
+	elif b<=a and b<=c:
+		return b
+	else:
+		return c
 
+def minEdit(str1,str2,str1_len,str2_len):
+	if str1_len==0:
+		return str2_len
+	if str2_len==0:
+		return str1_len
+
+	if str1[str1_len]==str2[str2_len]:
+		return minEdit(str1,str2,str1_len-1,str2_len-1)
+
+	return 1+getMin(
+		minEdit(str1,str2,str1_len,str2_len-1),
+		minEdit(str1,str2,str1_len-1,str2_len),
+		minEdit(str1,str2,str1_len-1,str2_len-1) )
+
+def getIndexOfMatch(str1,str2):
+	return minEdit(str1,str2,len(str1)-1,len(str2)-1)
+	
+def keyWordMatcher(keywords,target):
+	for i in keywords:
+		print(i , target , getIndexOfMatch(i,target))
 
 def readFile():
 	keywords=[]
@@ -15,6 +40,8 @@ def readFile():
 
 def main():
 	keywords = readFile()
+	target = input("Enter search query : ")
+	keyWordMatcher(keywords,target)
 
 
 if __name__ == "__main__":
